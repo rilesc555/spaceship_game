35a95ca0645226badc8871a0fd49017b8c6136ca
rilesc555 <rilesconnors555@gmail.com>
2025-01-29 14:50:15 -0600

    add camera

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use bevy::prelude::*;
 
+mod camera;
 mod debug;
 mod movement;
 mod spaceship;
 
+use camera::CameraPlugin;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
+
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
         .add_plugins(MovementPlugin)
         .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
         .add_plugins(DebugPlugin)
         .add_plugins(DefaultPlugins)
         .run();
--- src/spaceship.rs
+++ src/spaceship.rs
@@ -2,6 +2,15 @@ use bevy::prelude::*;
 
 use crate::movement::Velocity;
 
+const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+
+#[derive(Bundle)]
+struct SpaceshipBundle {
+    velocity: Velocity,
+    model: SceneBundle,
+}
+
 pub struct SpaceshipPlugin;
 
 impl Plugin for SpaceshipPlugin {
@@ -10,11 +19,15 @@ impl Plugin for SpaceshipPlugin {
     }
 }
 
-fn spawn_spaceship(mut commands: Commands) {
-    commands.spawn((
-        SpatialBundle::default(),
-        Velocity {
-            value: Vec3::new(0., 0., 0.),
+fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpaceshipBundle {
+        velocity: Velocity {
+            value: STARTING_VELOCITY,
         },
-    ));
+        model: SceneBundle {
+            scene: asset_server.load("Spaceship.glb#Scene0"),
+            transform: Transform::from_translation(STARTING_TRANSLATION),
+            ..default()
+        },
+    });
 }

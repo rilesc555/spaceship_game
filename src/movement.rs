926a2b25e7d6ac580098c36792d526b1e246ec13
rilesc555 <rilesconnors555@gmail.com>
2025-01-31 11:51:04 -0600

    asteroid spawning

--- src/movement.rs
+++ src/movement.rs
@@ -33,18 +33,18 @@ pub struct MovementPlugin;
 
 impl Plugin for MovementPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (update_position, update_velocity));
-    }
-}
-
-fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
-    for (velocity, mut transform) in query.iter_mut() {
-        transform.translation += velocity.value * time.delta_seconds();
+        app.add_systems(Update, (update_velocity, update_position));
     }
 }
 
 fn update_velocity(mut query: Query<(&Acceleration, &mut Velocity)>, time: Res<Time>) {
     for (acceleration, mut velocity) in query.iter_mut() {
         velocity.value += acceleration.value * time.delta_seconds();
+    }
+}
+
+fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        transform.translation += velocity.value * time.delta_seconds();
     }
 }

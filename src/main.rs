23c7eb5479d7fb6aa05c62a40ab5cc88f263fbd5
rilesc555 <rilesconnors555@gmail.com>
2025-02-04 14:14:20 -0600

    add website

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use bevy::prelude::*;
+
+mod asset_loader;
+mod asteroids;
+mod camera;
+mod collision_detection;
+mod debug;
+mod movement;
+mod spaceship;
+
+use asset_loader::AssetLoaderPlugin;
+use asteroids::AsteroidPlugin;
+use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
+use camera::CameraPlugin;
+use collision_detection::CollisionDetectionPlugin;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
+        .add_plugins(AssetLoaderPlugin)
+        .add_plugins(CollisionDetectionPlugin)
+        .add_plugins(AsteroidPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
+        .add_plugins(DebugPlugin)
+        // default plugins
+        .add_plugins(FrameTimeDiagnosticsPlugin)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
---
+++ src/spaceship.rs
@@ -0,0 +1,105 @@
+use bevy::{input::keyboard::KeyboardInput, prelude::*};
+
+use crate::{
+    asset_loader::{AssetLoaderPlugin, SceneAssets},
+    movement::{Acceleration, MovingObjectBundle, Velocity},
+};
+
+const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
+const SPACESHIP_SPEED: f32 = 25.0;
+const SPACESHIP_ROTATION_SPEED: f32 = 2.5;
+const SPACESHIP_ROLL_SPEED: f32 = 2.5;
+const MISSILE_SPEED: f32 = 50.0;
+const MISSILE_FORWARD_SPAWN: f32 = 7.5;
+
+#[derive(Component, Debug)]
+pub struct Spaceship;
+
+#[derive(Component, Debug)]
+pub struct SpaceshipMissile;
+
+pub struct SpaceshipPlugin;
+
+impl Plugin for SpaceshipPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PostStartup, spawn_spaceship).add_systems(
+            Update,
+            (spaceship_movement_controls, spaceship_weapon_controls),
+        );
+    }
+}
+
+fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
+    commands.spawn((
+        MovingObjectBundle {
+            acceleration: Acceleration::new(Vec3::ZERO),
+            velocity: Velocity::new(Vec3::ZERO),
+            model: SceneBundle {
+                scene: scene_assets.spaceship.clone(),
+                transform: Transform::from_translation(STARTING_TRANSLATION),
+                ..default()
+            },
+        },
+        Spaceship,
+    ));
+}
+
+fn spaceship_movement_controls(
+    mut query: Query<(&mut Transform, &mut Velocity), With<Spaceship>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (mut transform, mut velocity) = query.single_mut();
+    let mut rotation = 0.0;
+    let mut roll = 0.0;
+    let mut movement = 0.0;
+
+    if keyboard_input.pressed(KeyCode::A) {
+        rotation = SPACESHIP_ROTATION_SPEED * time.delta_seconds();
+    } else if keyboard_input.pressed(KeyCode::D) {
+        rotation = -SPACESHIP_ROTATION_SPEED * time.delta_seconds();
+    }
+
+    if keyboard_input.pressed(KeyCode::S) {
+        movement = -SPACESHIP_SPEED;
+    } else if keyboard_input.pressed(KeyCode::W) {
+        movement = SPACESHIP_SPEED;
+    }
+
+    if keyboard_input.pressed(KeyCode::ShiftLeft) {
+        roll = -SPACESHIP_ROLL_SPEED * time.delta_seconds();
+    } else if keyboard_input.pressed(KeyCode::ControlLeft) {
+        roll = SPACESHIP_ROLL_SPEED * time.delta_seconds();
+    }
+
+    transform.rotate_y(rotation);
+    transform.rotate_local_z(roll);
+
+    velocity.value = -transform.forward() * movement;
+}
+
+fn spaceship_weapon_controls(
+    mut commands: Commands,
+    query: Query<&Transform, With<Spaceship>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    scene_assets: Res<SceneAssets>,
+) {
+    let transform = query.single();
+    if keyboard_input.pressed(KeyCode::Space) {
+        commands.spawn((
+            MovingObjectBundle {
+                velocity: Velocity::new(-transform.forward() * MISSILE_SPEED),
+                acceleration: Acceleration::new(Vec3::ZERO),
+                model: SceneBundle {
+                    scene: scene_assets.missiles.clone(),
+                    transform: Transform::from_translation(
+                        transform.translation + -transform.forward() * MISSILE_FORWARD_SPAWN,
+                    )
+                    .with_scale(Vec3::new(0.15, 0.15, 0.15)),
+                    ..default()
+                },
+            },
+            SpaceshipMissile,
+        ));
+    }
+}
